92f3767365c41d19f37846da57644d0d4e63c8ff
ags-21ct <admin@prachakij.com>
2018-11-24 09:18:14 +0700

    This is a new commit for what I originally planned to be amended

---
+++ js/setfirebase.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.4.2/firebase.js"></script>
+    <script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyCuFbaRiuzD9To0mYDIlrC0cXUwXVWNQgI",
+    authDomain: "likecoin-2d12d.firebaseapp.com",
+    databaseURL: "https://likecoin-2d12d.firebaseio.com",
+    projectId: "likecoin-2d12d",
+    storageBucket: "likecoin-2d12d.appspot.com",
+    messagingSenderId: "254982663792"
+};
+firebase.initializeApp(config);
+</script>
